7d49c2cb564ec3be00d9a14bebcdae5fba77f9c0
Mohamed Omar <mohamed12@live.co.uk>
2024-12-30 01:08:55 +0000

    Add Birmingham prayer times fetching and API endpoint

--- api/timetable/timetable.ts
+++ api/timetable/timetable.ts
@@ -4,35 +4,11 @@ import { secret } from "encore.dev/config";
 import { db } from "./database";
 import { prayerTimes } from "./schema";
 import { PrayerTimes } from "./times";
+import { and, eq, sql } from "drizzle-orm";
 
-interface TimetableLondonParams {
+interface TimetableParams {
   year: number;
   month: string;
-}
-
-interface TimetableLondonResponse {
-  status: number | ErrCode;
-  body?: {
-    city: string;
-    times: {
-      [key: string]: {
-        asr: string;
-        asr_2: string;
-        asr_jamat: string;
-        date: string;
-        dhuhr: string;
-        dhuhr_jamat: string;
-        fajr: string;
-        fajr_jamat: string;
-        isha: string;
-        isha_jamat: string;
-        magrib: string;
-        magrib_jamat: string;
-        sunrise: string;
-      };
-    };
-  };
-  error?: string;
 }
 
 const LONDON_API_KEY = secret("LONDON_PRAYER_TIMES_API");
@@ -56,7 +32,7 @@ async function fetchLondonPrayerTimes(year: number, month: string) {
 
 export const collectFromLondon = api(
   { method: "GET", path: "/london/:year/:month" },
-  async (p: TimetableLondonParams) => {
+  async (p: TimetableParams) => {
     try {
       const result = await fetchLondonPrayerTimes(p.year, p.month);
 
@@ -100,7 +76,132 @@ export const collectFromLondon = api(
       }
       return {
         status: 500,
-        error
+        error,
+      };
+    }
+  }
+);
+
+async function fetchBirminghamPrayerTimes() {
+  const url = "https://centralmosque.org.uk/timetable/";
+  const response = await fetch(url);
+  const data = await response.text();
+
+  const regex = /(&nbsp;|<([^>]+)>)/gi;
+  const match = data.match(/<tbody>(.|\n)*<\/tbody>/);
+  if (!match) {
+    throw new APIError(
+      ErrCode.Internal,
+      "Failed to find tbody in the response"
+    );
+  }
+
+  const table = match[0];
+  const trArray = table.match(/<tr[\s\S]*?<\/tr>/g);
+
+  if (!trArray) {
+    throw new APIError(ErrCode.Internal, "Failed to parse table rows");
+  }
+
+  const parseTime = (time: string): string => {
+    return new Date(`2000-01-01 ${time}`).toLocaleTimeString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  };
+
+  const parseDate = (date: string): string => {
+    const cleanedDate = date.replace(/\b(\d+)(st|nd|rd|th)\b/, "$1");
+    return new Date(cleanedDate).toISOString().split("T")[0] ?? "";
+  };
+
+  return trArray.map((tr): Partial<PrayerTimes> => {
+    const tdMatch = tr.match(/<td[\s\S]*?<\/td>/g);
+    if (!tdMatch) {
+      throw new APIError(ErrCode.Internal, "Failed to parse table cells");
+    }
+    const tdArray = tdMatch.map((td) => td.replace(regex, ""));
+
+    // Using null coalescing to provide default values for missing fields
+    return {
+      locationId: 2,
+      date: parseDate(tdArray[0]),
+      fajr: parseTime(tdArray[2]),
+      fajrJamat: parseTime(tdArray[3]),
+      sunrise: parseTime(tdArray[4]),
+      dhuhr: parseTime(tdArray[6]),
+      dhuhrJamat: parseTime(tdArray[7]),
+      asr: parseTime(tdArray[8]), // Using asr instead of asr2 for consistency
+      asr2: parseTime(tdArray[8]),
+      asrJamat: parseTime(tdArray[9]),
+      maghrib: parseTime(tdArray[10]),
+      maghribJamat: parseTime(tdArray[10]), // Using maghrib time as jamat time if not provided
+      isha: parseTime(tdArray[12]),
+      ishaJamat: parseTime(tdArray[13]),
+    };
+  });
+}
+
+export const collectFromBirmingham = api(
+  { method: "GET", path: "/birmingham" },
+  async () => {
+    try {
+      const birminghamPrayerTimes =
+        (await fetchBirminghamPrayerTimes()) as PrayerTimes[];
+
+      const savedPrayerTimes = await db
+        .insert(prayerTimes)
+        .values(birminghamPrayerTimes)
+        .returning();
+
+      return {
+        status: ErrCode.OK,
+        body: savedPrayerTimes,
+      };
+    } catch (error) {
+      if (error instanceof APIError) {
+        return {
+          status: error.code,
+          error: error.message,
+        };
+      }
+      return {
+        status: 500,
+        error,
+      };
+    }
+  }
+);
+
+export const dropTimesByLocationAndMonthYear = api(
+  { method: "DELETE", path: "/times/location/:locationId/:year/:month" },
+  async (p: { locationId: number; year: number; month: number }) => {
+    const dateRangeTheYearMonthsStart = new Date(p.year, p.month - 1, 1);
+    const dateRangeTheYearMonthsEnd = new Date(p.year, p.month, 0);
+    try {
+      await db
+        .delete(prayerTimes)
+        .where(
+          and(
+            eq(prayerTimes.locationId, p.locationId),
+            sql`date >= ${dateRangeTheYearMonthsStart.toISOString()} AND date <= ${dateRangeTheYearMonthsEnd.toISOString()}`
+          )
+        );
+      return {
+        status: ErrCode.OK,
+        body: null,
+      };
+    } catch (error) {
+      if (error instanceof APIError) {
+        return {
+          status: error.code,
+          error: error.message,
+        };
+      }
+      return {
+        status: 500,
+        error,
       };
     }
   }
