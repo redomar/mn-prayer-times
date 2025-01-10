dbb8f5132f94c9b29c29eca7edd79a7f28b58ba7
Mohamed Omar <mohamed12@live.co.uk>
2025-01-10 17:30:56 +0000

    Add PostHog analytics integration with custom page view tracking

---
+++ src/app/providers.tsx
@@ -0,0 +1,28 @@
+// app/providers.tsx
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect } from "react";
+import PostHogPageView from "./PostHogPageView";
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  const KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "";
+  const HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+  useEffect(() => {
+    posthog.init(KEY, {
+      api_host: HOST,
+      capture_pageview: true, // Disable automatic pageview capture, as we capture manually
+      person_profiles: "always", // Enables capturing of person profiles
+    });
+  }, [KEY, HOST]);
+
+  console.log("PostHogProvider, KEY:", KEY);
+
+  return (
+    <PHProvider client={posthog}>
+      <PostHogPageView />
+      {children}
+    </PHProvider>
+  );
+}
