9983e76e568a2ea4c9321b53486ea0ea229c9a91
Mohamed Omar <mohamed12@live.co.uk>
2025-01-31 13:50:38 +0000

    Update Birmingham prayer time collection to use dynamic month and year in endpoint parameters

--- api/timetable/cron.ts
+++ api/timetable/cron.ts
@@ -4,7 +4,11 @@ import { collectFromBirmingham } from "./timetable";
 const _ = new CronJob("CollectBirmingham", {
   title: "Collect prayer times for Birmingham",
   schedule: "0 9 1 * *",
-  endpoint: collectFromBirmingham,
+  endpoint: () =>
+    collectFromBirmingham({
+      month: new Date().toLocaleString("en-GB", { month: "long" }),
+      year: new Date().getFullYear().toString(),
+    }),
 });
 
 export default _;
